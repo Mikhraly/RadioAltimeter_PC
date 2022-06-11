82cc49fe4a3346002e1d493556450de412261f9a
Mikhraly <mikhraly@gmail.com>
2022-06-11 22:04:45 +0300

    add start text in MyForm.cpp

--- ComPort/MyForm.cpp
+++ ComPort/MyForm.cpp
@@ -1,2 +1,11 @@
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ComPort::MyForm form;
+	Application::Run(% form);
+}
