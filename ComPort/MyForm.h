916eeaec97564ec4f3b292e3ccc2cc5d3fd6eaa7
Mikhraly <mikhraly@gmail.com>
2022-06-11 22:57:26 +0300

    add buttonOpenPort

--- ComPort/MyForm.h
+++ ComPort/MyForm.h
@@ -8,6 +8,7 @@ namespace ComPort {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO::Ports;
 
 	/// <summary>
 	/// Сводка для MyForm
@@ -34,7 +35,10 @@ namespace ComPort {
 				delete components;
 			}
 		}
-	private: System::IO::Ports::SerialPort^ serialPort;
+
+
+	private: SerialPort^ port;
+	private: System::Windows::Forms::Button^ buttonOpenPort;
 	protected:
 
 	protected:
@@ -53,26 +57,34 @@ namespace ComPort {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
-			this->serialPort = (gcnew System::IO::Ports::SerialPort(this->components));
+			this->buttonOpenPort = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
-			// serialPort
+			// buttonOpenPort
 			// 
-			this->serialPort->PortName = L"COM1";
-			this->serialPort->BaudRate = 115200;
-			this->serialPort->DataBits = 8;
+			this->buttonOpenPort->Location = System::Drawing::Point(96, 112);
+			this->buttonOpenPort->Name = L"buttonOpenPort";
+			this->buttonOpenPort->Size = System::Drawing::Size(100, 30);
+			this->buttonOpenPort->TabIndex = 0;
+			this->buttonOpenPort->Text = L"OpenPort";
+			this->buttonOpenPort->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->buttonOpenPort);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+
+
+		void openSerialPort() {
+			port->Open();
+		}
 	};
 }
